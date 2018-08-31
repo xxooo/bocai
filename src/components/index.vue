b5dfa4c7bdda4a6c59c13e12e1070a0da164fd0a
xuyao <tim.xu@jwis.cn>
2018-08-31 17:59:19 +0800

    H Signed-off-by: xuyao <tim.xu@jwis.cn>

---
+++ src/components/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- <page-header ></page-header>
+    <left-panel  ></left-panel> -->
+    <div id="main">
+        <router-view :user="user"></router-view>
+        <!-- <page-footer></page-footer> -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+// import PageHeader from 'components/common/pageheader';
+// import LeftPanel from 'components/common/leftpanel';
+// import PageFooter from 'components/common/pagefooter';
+import {mapState,mapGetters} from 'vuex';
+
+export default {
+  components: {
+    // PageHeader,
+    // LeftPanel,
+    // PageFooter
+  },
+  data() {
+    return {
+      user:{}
+    }
+  },
+  async created() {
+  },
+  computed: {
+    ...mapGetters({
+    })
+  },
+  methods: {
+  },
+  mounted() {
+  },
+  updated() {
+  }
+};
+</script>
+<style lang="less">
+</style>
