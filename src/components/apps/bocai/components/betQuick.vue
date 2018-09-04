fb50d7935be037b31e0c50daf72dfa43bad40e56
xuyao <tim.xu@jwis.cn>
2018-09-04 17:55:30 +0800

    wef Signed-off-by: xuyao <tim.xu@jwis.cn>

---
+++ src/components/apps/bocai/components/betQuick.vue
@@ -0,0 +1,102 @@
+<template>
+	<div class="betQuick">
+    <div class="beishu"></div>
+    <div class="betRight">
+      <div class="betRTop">
+        <el-input v-model="money" size="mini" placeholder="请输入金额"></el-input>
+        <el-button type="primary"size="mini" plain>下 注</el-button>
+        <el-button type="danger" size="mini">重 置</el-button>
+      </div>
+      <div class="betRBottom">
+        <label>投注类型:</label>
+        <el-radio-group v-model="radio10" size="mini">
+          <el-radio label="1" border>快捷</el-radio>
+          <el-radio label="2" border>一般</el-radio>
+        </el-radio-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import { mapGetters } from 'vuex';
+
+	export default {
+		props: {
+      isbetType: true
+		},
+		data() {
+			return {
+        money: '',
+        radio10: '1'
+			}
+		},
+    components: {
+		},
+		async created() {
+    },
+    computed:{
+      ...mapGetters({
+      })
+    },
+    mounted(){
+    },
+		methods: {
+		}
+	}
+
+</script>
+
+<style scoped>
+.betQuick {
+    text-align: center;
+    margin-bottom: 20px;
+    margin-top: 14px;
+    position: relative;
+    height: 67px;
+}
+
+.betQuick div {
+  display: inline-block;
+  float: left;
+  margin-left: 20px;
+}
+
+.beishu {
+  width: 400px;
+  height: 67px;
+  background: url(./../../../../../static/img/casino-wt.png) bottom no-repeat transparent;
+}
+
+.betRight {
+  width: 380px;
+  margin-left: 50px;
+}
+
+.betRBottom label {
+  color: #fff;
+  float: left;
+  padding: 5px 20px 0px 20px;
+}
+
+.betRBottom {
+  margin-top: 10px;
+}
+
+</style>
+
+<style lang="less">
+
+.betQuick {
+  .el-input {
+    width: 120px;
+    margin-right: 20px;
+  }
+
+  .el-radio.is-bordered.is-checked {
+    background-color: #ffd04b;
+  }
+}
+
+</style>
---
+++ src/components/apps/bocai/components/clockTimes.vue
@@ -0,0 +1,112 @@
+<template>
+	<div id="clockTimes">
+    <center>
+    <span id="clock">{{timeLeft}}</span>
+    </center>
+  </div>
+</template>
+
+
+<script>
+  import { mapGetters } from 'vuex';
+
+	export default {
+		props: {
+		},
+		data() {
+			return {
+        timeLeft:''
+			}
+		},
+    components: {
+		},
+		async created() {
+      this.gettimeLeft();
+    },
+    computed:{
+          ...mapGetters({
+          })
+     },
+    mounted(){
+     },
+		methods: {
+      gettimeLeft() {
+        var hourtime="24:00:00";
+        var endTime =new Date(new Date().toLocaleDateString()+" "+hourtime);
+
+        var now = new Date();
+
+        var nowYear=now.getFullYear();
+        var nowMonth=now.getMonth()+1;
+        var nowDay=now.getDate();
+
+        var leftTime = endTime.getTime() - now.getTime();
+        var ms = parseInt(leftTime % 1000).toString();
+        leftTime = parseInt(leftTime / 1000);
+        var o = Math.floor(leftTime / 3600);
+        var d = Math.floor(o / 24);
+        var m = Math.floor(leftTime / 60 % 60);
+        var s = leftTime % 60;
+
+        this.timeLeft = o + ":" + m + ":" + s;
+
+        setTimeout(this.gettimeLeft, 100);
+      }
+		}
+	}
+
+</script>
+
+<style scoped>
+#clock {
+  font: bold 24pt sans;
+  background-color: #f5f5f5;
+  padding: 0px 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin: 0px 0px 0px 40px;
+  color: #130e0e;
+}
+
+div.dig, div.hour, div.min, div.sec
+    {
+      position:absolute;
+    }
+  div.hour, div.min, div.sec
+    {
+    width:2px;
+    height:2px;
+    font-size:2px;
+    }
+    div.dig
+    {
+    width:30px;
+    height:30px;
+    font-family:arial,verdana,sans-serif;
+    font-size:10px;
+    color:#000000;
+    text-align:center;
+    padding-top:10px
+    }
+    div.min
+    {
+    background:#0000FF;
+    }
+    div.hour
+    {
+    background:#000000;
+    }
+    div.sec
+    {
+    background:#FF0000;
+    }
+
+</style>
+
+<style lang="less">
+#clockTimes {
+  
+
+}
+
+</style>
