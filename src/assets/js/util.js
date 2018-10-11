422774fb8ae8c25a60d86dffe3f00ee44380e8c2
‘xuyao’ <tim.xu@jwis.cn>
2018-10-11 23:57:28 +0800

    上上上
    Signed-off-by: ‘xuyao’ <‘tim.xu@jwis.cn’>

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -55,6 +55,17 @@ export default {
           });
         };
 
+        Vue.prototype.$timestampToTime = function(timestamp){
+            var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+            var Y = date.getFullYear() + '-';
+            var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+            var D = date.getDate() + ' ';
+            var h = date.getHours() + ':';
+            var m = date.getMinutes() + ':';
+            var s = date.getSeconds();
+            return Y+M+D+h+m+s;
+        };
+
         Vue.prototype.$currUser = async function(){
             currUser = currUser || JSON.parse(cookie.get('currUser'));
             if(currUser) return currUser;
