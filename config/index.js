f02394f6d8f368623120d80ef78e6e75f664ed59
‘xuyao’ <tim.xu@jwis.cn>
2019-04-03 00:14:29 +0800

    wefew
    Signed-off-by: ‘xuyao’ <‘tim.xu@jwis.cn’>

--- config/index.js
+++ config/index.js
@@ -13,8 +13,9 @@ module.exports = {
     proxyTable: {
         '/': {
           // 测试环境
-          target: 'http://47.106.13.12:8008',  // 接口域名
+          //target: 'http://47.106.13.12:8008',  // 接口域名
           //target: 'http://192.168.0.105:8888',  // 接口域名
+          target: 'http://192.168.1.109:8889',  // 接口域名
           changeOrigin: true,  //是否跨域
           pathRewrite: {
               '^/': ''   //需要rewrite重写的,
